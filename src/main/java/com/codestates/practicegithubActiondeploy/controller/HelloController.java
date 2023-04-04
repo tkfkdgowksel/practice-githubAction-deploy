6706e887a732ec586b582501beb2e4266e23a834
tkfkdgowksel <tkfkdgowksel@gmail.com>
2023-04-04 14:42:05 +0900

    Update HelloController.java

--- src/main/java/com/codestates/practicegithubActiondeploy/controller/HelloController.java
+++ src/main/java/com/codestates/practicegithubActiondeploy/controller/HelloController.java
@@ -3,7 +3,7 @@ package com.codestates.practicegithubActiondeploy.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Restcontroller
+@RestController
 public class HelloController{
   @GetMapping("/")
   public String hello(){
